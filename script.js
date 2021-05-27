d789511d4ab8955ffd4043f78febde861e492350
mhousetree <mhousetree@gmail.com>
2021-05-28 08:13:13 +0900

    :sparkles: 任意の背景色変更機能を追加

--- script.js
+++ script.js
@@ -25,7 +25,40 @@ body.addEventListener(
     { once: true }
 );
 
-// set a function for question and answer when name is hovered
+// set colors on color pallet
+const color_pallet = document.querySelector("ul");
+
+function toggleColorPallet() {
+    const is_open_check = document.querySelector("input[name='button_toggle']");
+    if (is_open_check.checked) {
+        color_pallet.style.opacity = "0";
+        setTimeout(function(){color_pallet.style.visibility = 'hidden';}, 400);
+    } else {
+        color_pallet.style.visibility = "visible";
+        color_pallet.style.opacity = "1";
+    }
+    is_open_check.checked = !is_open_check.checked;
+}
+
+for (var i = 0; i < color_pallet.children.length; i++) {
+    color_pallet.children[i].style.backgroundColor = backgroundColorList[i];
+    color_pallet.children[i].addEventListener(
+        'click',
+        function() {
+            body.style.backgroundColor = this.style.backgroundColor;
+            toggleColorPallet();
+        }
+    );
+}
+
+// set a function for selection of colors
+
+const color_toggle_button = document.getElementById("bg_color_change");
+
+color_toggle_button.addEventListener('click', toggleColorPallet);
+
+
+// set a function for question and answer which work when the application name is hovered
 const qa_target = document.getElementById("hover_qa");
 
 qa_target.addEventListener(
